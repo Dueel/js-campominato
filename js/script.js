0f4f684d3f559d8542802f16f4bafdb7e2cc6eb1
Dueel <diego.bar@live.it>
2021-03-16 15:29:06 +0100

    Input per scatenare le funzioni

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var level = parseInt(prompt('Seleziona la difficoltá'));
 
+document.getElementById('button').addEventListener('click', function(){
+    
+    level = parseInt(prompt('Seleziona la difficoltá'));
+    arrayBot = picker(16, level);
+    arrayUser = userNumbers(level);
+    console.log('Array Bot: ' + arrayBot);
+    console.log('Array User: ' + arrayUser);
+
+})
 
 // Funzione "picker" genera dei numeri casuali in base al grado di difficoltá
 
@@ -8,51 +16,48 @@ function picker(numPicked, limit){
     var array = [];
 
     while (array.length != numPicked){
-
-        var numRandom = Math.floor(Math.random() * limit + 1);
-
-        if (!array.includes(numRandom)){
         
+        var numRandom = Math.floor(Math.random() * limit + 1);
+        
+        if (!array.includes(numRandom)){
+            
             array.push(numRandom);
         }
     }
-
+    
     return array;
 }
-
-var arrayBot = picker(16, level);
-console.log(arrayBot);
 
 
 // Funzione "userNumbers" chiede all'utente il livello di difficoltá e i numeri da inserire
 
 function userNumbers(limit){
-
+    
     var difference = limit - arrayBot.length;
     var array = [];
-
+    
     while (array.length != difference){
-
+        
         var numUser = parseInt(prompt('Inserisci un numero tra 1 e ' + limit));
-
+        
         console.log(numUser);
-
+        
         if (!array.includes(numUser)){
             array.push(numUser);
-
+            
         } else {
             alert('Hai giá inserito questo numero');
-
+            
         }
-
+        
         if (arrayBot.includes(numUser)){
-
+            
             console.log('hai perso');
             break;
         }
     }
-
+    
     return array
 }
 
-var arrayUser = userNumbers(level);
+
